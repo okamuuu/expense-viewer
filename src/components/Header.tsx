897cc4e6019af955f4b6e5fd1150702ece0d0c59
okamuuu <okamuuu@users.noreply.github.com>
2025-02-13 12:29:34 +0900

    chore!: version up (#15)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export const Header = () => {
                 EXPENSE VIEWER
               </div>
               <div className="text-blue-600 text-xs font-medium pt-1">
-                v1.0.1
+                v1.0.2
               </div>
             </div>
             <div>
